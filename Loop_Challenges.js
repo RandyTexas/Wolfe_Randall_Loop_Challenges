ee7929dd4342b6d0faa2ad374c7767bbc73a6eec
Randall Wolfe <randywolfe12@gmail.com>
2023-11-14 10:58:17 -0600

    inital commit

---
+++ Loop_Challenges.js
@@ -0,0 +1,13 @@
+
+// Print odds 1-20
+for(let i =1; i <=20; i++ ){
+    if(i % 2 == 1)
+    console.log(i)
+}
+
+// Decreasing Multiples of 3
+for(let i =100; i>0; i--){
+    if(i % 3 ==0 ){
+        console.log(i)
+    }
+}
