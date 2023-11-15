05bf9986f60d0a92c84850c5cc47b5ec07a25347
Randall Wolfe <randywolfe12@gmail.com>
2023-11-15 14:40:34 -0600

    11/14/23

--- Loop_Challenges.js
+++ Loop_Challenges.js
@@ -1,13 +1,18 @@
 
-// Print odds 1-20
-for(let i =1; i <=20; i++ ){
-    if(i % 2 == 1)
-    console.log(i)
-}
+// // Print odds 1-20
+// for(let i =1; i <=20; i++ ){
+//     if(i % 2 == 1)
+//     console.log(i)
+// }
 
-// Decreasing Multiples of 3
-for(let i =100; i>0; i--){
-    if(i % 3 ==0 ){
-        console.log(i)
-    }
+// // Decreasing Multiples of 3
+// for(let i =100; i>0; i--){
+//     if(i % 3 ==0 ){
+//         console.log(i)
+//     }
+// }
+
+// Print the sequence
+for(let i = 4; i >-4; i=-1.5){
+    console.log(i)
 }
