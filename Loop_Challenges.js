7da001992495330e6a52b6b52bda21b0a8378bea
Randall Wolfe <randywolfe12@gmail.com>
2023-11-15 14:40:53 -0600

    11/14/23

--- Loop_Challenges.js
+++ Loop_Challenges.js
@@ -12,7 +12,23 @@
 //     }
 // }
 
-// Print the sequence
-for(let i = 4; i >-4; i=-1.5){
-    console.log(i)
+// // Print the sequence
+// for(let i = 4; i >-4; i -= 1.5){
+//     console.log(i)
+// }
+
+// // Sigma
+// let sum = 0;
+// for(let i = 1; i <=100; i++){
+//     sum += i
+// }
+// console.log(sum)
+// // I think this is what it was asking?
+
+// Factorial
+let product = 1;
+for(let i = 1; i <=12; i++){
+    product *= i
 }
+console.log(product)
+// it was like the Sigma one just a little different!
